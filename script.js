2808326fe66cdd1470d4d071cf8057fb843dbf6d
Ricardo <ri.tapiasaez@gmail.com>
2023-08-06 15:28:44 -0400

    refactoring

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-'use strict';
+"use strict";
+
+const btnShowModal = document.querySelectorAll(".show-modal");
+const btnCloseModal = document.querySelector(".close-modal");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+const openModal = function () {
+  console.log("Button clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnShowModal.length; i++)
+  btnShowModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
