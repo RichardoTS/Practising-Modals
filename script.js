a736948f62f577cde67b7ba3a91cd57f89e4dbb2
Ricardo <ri.tapiasaez@gmail.com>
2023-08-06 15:51:16 -0400

    finished

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 
 const openModal = function () {
-  console.log("Button clicked");
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
@@ -21,3 +20,9 @@ for (let i = 0; i < btnShowModal.length; i++)
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
